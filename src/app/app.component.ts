3006710f51fd59d8dbf5204e99bb7c1e6ec25d17
Adriano <adriano16_gubert@hotmail.com>
2020-04-27 20:25:56 -0300

    ExFireBase

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'ExemploFirebase';
+
+  constructor(private firestore: AngularFirestore){ }
+
+  ngOnInit(): void {
+
+    this.firestore.collection('produtos').get()
+      .toPromise()
+      .then(x => {
+        console.log('certo');
+        console.log(x);
+      })
+      .catch(erro => {
+        console.log(erro);
+      });
+
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDk72iR0-JkMzf-r_MAXBUd6C8xHSIgfM8",
+    authDomain: "exemplofirebase-2fc29.firebaseapp.com",
+    databaseURL: "https://exemplofirebase-2fc29.firebaseio.com",
+    projectId: "exemplofirebase-2fc29",
+    storageBucket: "exemplofirebase-2fc29.appspot.com",
+    messagingSenderId: "1045940326413",
+    appId: "1:1045940326413:web:2d654ec84a3f54d5dfc544",
+    measurementId: "G-QTNQCH0C9P"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
